b1d5b737a6cd56bb1009fd26a3497a2c76815b31
Cartopol <onmogelijk@gmail.com>
2020-04-26 10:39:26 +0200

    Àdds Eyes Wide Open and ZigZag

--- src/main/java/the_warlord/cards/warlord/EyesWideOpen.java
+++ src/main/java/the_warlord/cards/warlord/EyesWideOpen.java
@@ -12,24 +12,22 @@ import the_warlord.WarlordMod;
 import the_warlord.cards.CustomWarlordModCard;
 import the_warlord.characters.Warlord;
 
-public class Syrette extends CustomWarlordModCard {
-    public static final String ID = WarlordMod.makeID(Syrette.class.getSimpleName());
+public class EyesWideOpen extends CustomWarlordModCard {
+    public static final String ID = WarlordMod.makeID(EyesWideOpen.class.getSimpleName());
 
-    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Warlord.Enums.WARLORD_CARD_COLOR;
 
     private static final int COST = 0;
 
-    private static final int DEXTERITY = 1;
+    private static final int DEXTERITY = 2;
     private static final int UPGRADE_PLUS_DEXTERITY = 1;
 
-    public Syrette() {
+    public EyesWideOpen() {
         super(ID, COST, TYPE, COLOR, RARITY, TARGET);
         this.magicNumber = this.baseMagicNumber = DEXTERITY;
-        this.selfRetain = true;
-
     }
 
     @Override
