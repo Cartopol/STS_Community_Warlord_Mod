47f8adbc9466a5cb86ad181be6f0344a67e41b8e
Cartopol <onmogelijk@gmail.com>
2020-04-26 14:10:58 +0200

    Implements 2 commons, Me First and Wide Swipes.C

---
+++ src/main/java/the_warlord/cards/warlord/MeFirst.java
@@ -0,0 +1,51 @@
+package the_warlord.cards.warlord;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import the_warlord.WarlordMod;
+import the_warlord.cards.CustomWarlordModCard;
+import the_warlord.characters.Warlord;
+import the_warlord.util.IntentUtils;
+
+public class MeFirst extends CustomWarlordModCard {
+    public static final String ID = WarlordMod.makeID(MeFirst.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Warlord.Enums.WARLORD_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 10;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+    public MeFirst() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        this.isMultiDamage = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (IntentUtils.isAttackIntent(m.intent)) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage), AttackEffect.SLASH_HORIZONTAL));
+        }
+    }
+
+    @Override
+    public boolean shouldGlowGold() {
+        return IntentUtils.playerCanSeeThatAnyEnemyIntentMatches(IntentUtils::isAttackIntent);
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeDescription();
+        }
+    }
+}
---
+++ src/main/java/the_warlord/cards/warlord/WideSwipes.java
@@ -0,0 +1,50 @@
+package the_warlord.cards.warlord;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.DaggerSprayEffect;
+import the_warlord.WarlordMod;
+import the_warlord.cards.CustomWarlordModCard;
+import the_warlord.characters.Warlord;
+
+public class WideSwipes extends CustomWarlordModCard {
+    public static final String ID = WarlordMod.makeID(WideSwipes.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Warlord.Enums.WARLORD_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 3;
+    private static final int UPGRADE_PLUS_DMG = 1;
+    private static final int NB_HITS = 3;
+
+    public WideSwipes() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = NB_HITS;
+        this.isMultiDamage = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < NB_HITS; ++i) {
+            this.addToBot(new VFXAction(new DaggerSprayEffect(AbstractDungeon.getMonsters().shouldFlipVfx()), 0.0F));
+            this.addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AttackEffect.NONE));
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeDescription();
+        }
+    }
+}
