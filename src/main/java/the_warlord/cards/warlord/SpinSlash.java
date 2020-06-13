25a5f94131bd10a97ce906d92e730d73249d4a9c
Cartopol <onmogelijk@gmail.com>
2020-06-13 17:13:16 +0200

    Rebalance of Spin Slash

--- src/main/java/the_warlord/cards/warlord/SpinSlash.java
+++ src/main/java/the_warlord/cards/warlord/SpinSlash.java
@@ -18,9 +18,9 @@ public class SpinSlash extends CustomWarlordModCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Warlord.Enums.WARLORD_CARD_COLOR;
 
-    private static final int COST = 1;
-    private static final int DAMAGE = 5;
-    private static final int UPGRADE_PLUS_DAMAGE = 1;
+    private static final int COST = 2;
+    private static final int DAMAGE = 7;
+    private static final int UPGRADE_PLUS_DAMAGE = 2;
     private static final int NB_ATTACKS = 3;
     private static final int NB_DIZZY = 1;
 
