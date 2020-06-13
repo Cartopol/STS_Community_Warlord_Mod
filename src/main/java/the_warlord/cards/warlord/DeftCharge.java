533bee217bbe12ef10242d45351914594f7f805f
Cartopol <onmogelijk@gmail.com>
2020-06-13 17:12:19 +0200

    Rework of Deft Charge, rebalance of Zig Zag and Inner Balance

--- src/main/java/the_warlord/cards/warlord/DeftCharge.java
+++ src/main/java/the_warlord/cards/warlord/DeftCharge.java
@@ -5,7 +5,6 @@ import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import the_warlord.WarlordMod;
 import the_warlord.cards.CustomWarlordModCard;
@@ -20,13 +19,10 @@ public class DeftCharge extends CustomWarlordModCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Warlord.Enums.WARLORD_CARD_COLOR;
 
-    private static final int COST = 2;
-    private static final int DAMAGE = 14;
-    private static final int UPGRADE_PLUS_DAMAGE = 4;
-    private static final int DRAW = 2;
-    private static final int PLUS_DRAW = 1;
-
-
+    private static final int COST = 1;
+    private static final int DAMAGE = 7;
+    private static final int UPGRADE_PLUS_DAMAGE = 3;
+    private static final int DRAW = 1;
 
     public DeftCharge() {
         super(ID, COST, TYPE, COLOR, RARITY, TARGET);
@@ -38,14 +34,14 @@ public class DeftCharge extends CustomWarlordModCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage), AbstractGameAction.AttackEffect.SMASH));
         if (p.hasPower(TensionPower.POWER_ID)) {
-            addToBot(new DrawCardAction(magicNumber));
+            addToBot(new DrawCardAction(magicNumber * p.getPower(TensionPower.POWER_ID).amount));
         }
     }
-
-    @Override
-    public boolean shouldGlowGold() {
-        return AbstractDungeon.player.hasPower(TensionPower.POWER_ID);
-    }
+//
+//    @Override
+//    public boolean shouldGlowGold() {
+//        return AbstractDungeon.player.hasPower(TensionPower.POWER_ID);
+//    }
 
     @Override
     public void upgrade() {
--- src/main/java/the_warlord/cards/warlord/ZigZag.java
+++ src/main/java/the_warlord/cards/warlord/ZigZag.java
@@ -20,7 +20,7 @@ public class ZigZag extends CustomWarlordModCard {
     private static final int COST = 1;
     private static final int BLOCK = 4;
     private static final int UPGRADE_PLUS_BLOCK = 2;
-    private static final int BLOCK_PER_TENSION = 2;
+    private static final int BLOCK_PER_TENSION = 3;
     private static final int UPGRADE_PLUS_BLOCK_PER_TENSION = 1;
 
 
